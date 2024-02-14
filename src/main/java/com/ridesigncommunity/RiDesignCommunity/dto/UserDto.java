4759bae25242216d2c5ca23cfc0a39d1c20bc3c8
RiDumoulin <riannedumoulin@gmail.com>
2024-02-14 13:41:58 +0100

    Dto en model User gewerkt

--- src/main/java/com/ridesigncommunity/RiDesignCommunity/dto/UserDto.java
+++ src/main/java/com/ridesigncommunity/RiDesignCommunity/dto/UserDto.java
@@ -1,4 +1,20 @@
 package com.ridesigncommunity.RiDesignCommunity.dto;
 
 public class UserDto {
+    @NotBlank
+    public String firstName;
+
+    @NotBlank
+    public String lastName;
+
+    @NotBlank
+    public String userName;
+
+    @NotBlank
+    public String email;
+
+    @NotBlank
+    @Size(min=7, max=30)
+    public String password;
+
 }
--- src/main/java/com/ridesigncommunity/RiDesignCommunity/model/User.java
+++ src/main/java/com/ridesigncommunity/RiDesignCommunity/model/User.java
@@ -1,4 +1,112 @@
 package com.ridesigncommunity.RiDesignCommunity.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "user_name")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_ri_designer")
+    private boolean isRiDesigner;
+
+    @OneToMany(mappedBy = "user")
+    private List<Product> favorites;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isRiDesigner() {
+        return isRiDesigner;
+    }
+
+    public void setRiDesigner(boolean isRiDesigner) {
+        this.isRiDesigner = isRiDesigner;
+    }
+
+    public List<Product> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Product> favorites) {
+        this.favorites = favorites;
+    }
+
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+
 }
